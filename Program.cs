d210d6c39640927547e42497cb17b41085f78dd0
Gerson Hernandez <ghernandez@GrupoTerra.com>
2020-02-20 21:02:08 -0600

    Iniciando el proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentos_Parte_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
